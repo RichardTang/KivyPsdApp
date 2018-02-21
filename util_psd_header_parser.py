2293b78c332b0f56819612c84e99bfe34cb6177b
RichardTang <e5527c@gmail.com>
2018-02-21 12:05:00 +0800

    code format

--- util_psd_header_parser.py
+++ util_psd_header_parser.py
@@ -15,48 +15,47 @@ from util_file_parser import FileParser
 from psd_modes import MODES
 
 class PsdHeaderParser(FileParser):
-    header = None
+  header = None
   
-  
-    def parse_header(self):
-        Logger.info("")
-        Logger.info("# Header #")
+  def parse_header(self):
+    Logger.info("")
+    Logger.info("# Header #")
 
-        self.header = {}
+    self.header = {}
 
-        C_PSD_HEADER = ">4sH 6B HLLHH"
-        (
-            self.header['sig'],
-            self.header['version'],
-            self.header['r0'],
-            self.header['r1'],
-            self.header['r2'],
-            self.header['r3'],
-            self.header['r4'],
-            self.header['r5'],
-            self.header['channels'],
-            self.header['rows'],
-            self.header['cols'],
-            self.header['depth'],
-            self.header['mode']
-        ) = self._readf(C_PSD_HEADER)
+    C_PSD_HEADER = ">4sH 6B HLLHH"
+    (
+      self.header['sig'],
+      self.header['version'],
+      self.header['r0'],
+      self.header['r1'],
+      self.header['r2'],
+      self.header['r3'],
+      self.header['r4'],
+      self.header['r5'],
+      self.header['channels'],
+      self.header['rows'],
+      self.header['cols'],
+      self.header['depth'],
+      self.header['mode']
+    ) = self._readf(C_PSD_HEADER)
 
-        self.size = [self.header['rows'], self.header['cols']]
-        #Logger.info("1111111")
+    self.size = [self.header['rows'], self.header['cols']]
+    #Logger.info("1111111")
 
-        if self.header['sig'] != "8BPS":
-            raise ValueError("Not a PSD signature: '%s'" % self.header['sig'])
-        if self.header['version'] != 1:
-            raise ValueError("Can not handle PSD version:%d" % self.header['version'])
-        self.header['modename'] = MODES[self.header['mode']] if 0 <= self.header['mode'] < 16 else "(%s)" % self.header['mode']
-        #Logger.info("22222222")
+    if self.header['sig'] != "8BPS":
+      raise ValueError("Not a PSD signature: '%s'" % self.header['sig'])
+    if self.header['version'] != 1:
+      raise ValueError("Can not handle PSD version:%d" % self.header['version'])
+    self.header['modename'] = MODES[self.header['mode']] if 0 <= self.header['mode'] < 16 else "(%s)" % self.header['mode']
+    #Logger.info("22222222")
 
-        Logger.info(INDENT_OUTPUT(1, "channels:%(channels)d, rows:%(rows)d, cols:%(cols)d, depth:%(depth)d, mode:%(mode)d [%(modename)s]" % self.header))
-        Logger.info(INDENT_OUTPUT(1, "%s" % self.header))
+    Logger.info(INDENT_OUTPUT(1, "channels:%(channels)d, rows:%(rows)d, cols:%(cols)d, depth:%(depth)d, mode:%(mode)d [%(modename)s]" % self.header))
+    Logger.info(INDENT_OUTPUT(1, "%s" % self.header))
 
-        # Remember position
-        self.header['colormodepos'] = self.fd.tell()
-        self._skip_block("color mode data", 1)
+    # Remember position
+    self.header['colormodepos'] = self.fd.tell()
+    self._skip_block("color mode data", 1)
 
 
   
