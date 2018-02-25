cb9ed941f3dd9e84578363722eb5b7d8772dbfcf
RichardTang <e5527c@gmail.com>
2018-02-25 12:39:02 +0800

    add parse descriptor

--- util_psd_layer_image_parser.py
+++ util_psd_layer_image_parser.py
@@ -16,74 +16,75 @@ from psd_modes import MODES
 from util_psd_image_resource_parser import PsdImageResourceParser
 
 class PsdLayerImageParser(PsdImageResourceParser):
-    def _read_descriptor(self):
-        # Descriptor
-        def _unicode_string():
-            len = self._readf(">L")[0]
-            result = u''
-            for count in range(len):
-                val = self._readf(">H")[0]
-                if val:
-                    result += unichr(val)
-            return result
+  def _unicode_string(self):
+    len = self._readf(">L")[0]
+    result = u''
+    for count in range(len):
+      val = self._readf(">H")[0]
+      if val:
+        result += unichr(val)
+    return result
+    
+  def _desc_TEXT(self):
+    return self._unicode_string()
 
-        def _string_or_key():
-            len = self._readf(">L")[0]
-            if not len:
-                len = 4
-            return self._readf(">%ds" % len)[0]
+  def _desc_enum(self):
+    return { 
+      'typeID' : self._string_or_key(),
+        'enum' : self._string_or_key(),
+           }
 
-        def _desc_TEXT():
-            return _unicode_string()
+  def _desc_long(self):
+    return self._readf(">l")[0]
 
-        def _desc_enum():
-            return { 'typeID' : _string_or_key(),
-                     'enum' : _string_or_key(),
-                     }
+  def _desc_bool(self):
+    return self._readf(">?")[0]
 
-        def _desc_long():
-            return self._readf(">l")[0]
+  def _desc_doub(self):
+    return self._readf(">d")[0]
 
-        def _desc_bool():
-            return self._readf(">?")[0]
+  def _string_or_key(self):
+    len = self._readf(">L")[0]
+    if not len:
+      len = 4
+    return self._readf(">%ds" % len)[0]
 
-        def _desc_doub():
-            return self._readf(">d")[0]
+  def _desc_tdta():
+    # Apparently it is pdf data?
+    # http://telegraphics.com.au/svn/psdparse
+    # descriptor.c pdf.c
 
-        def _desc_tdta():
-            # Apparently it is pdf data?
-            # http://telegraphics.com.au/svn/psdparse
-            # descriptor.c pdf.c
+    len = self._readf(">L")[0]
+    pdf_data = self.fd.read(len)
+    return pdf_data
+    
+  def _read_descriptor(self):
+    # Descriptor
+    _desc_item_factory = {
+      'TEXT' : self._desc_TEXT,
+      'enum' : self._desc_enum,
+      'long' : self._desc_long,
+      'bool' : self._desc_bool,
+      'doub' : self._desc_doub,
+      'tdta' : self._desc_tdta,
+      #'Objc' : _desc_tdta,#TODO:
+    }
 
-            len = self._readf(">L")[0]
-            pdf_data = self.fd.read(len)
-            return pdf_data
+    class_id_name = self._unicode_string()
+    class_id = self._string_or_key()
+    Logger.info(INDENT_OUTPUT(4, u"name='%s' clsid='%s'" % (class_id_name, class_id)))
 
-        _desc_item_factory = {
-            'TEXT' : _desc_TEXT,
-            'enum' : _desc_enum,
-            'long' : _desc_long,
-            'bool' : _desc_bool,
-            'doub' : _desc_doub,
-            'tdta' : _desc_tdta,
-            #'Objc' : _desc_tdta,#TODO:
-            }
+    item_count = self._readf(">L")[0]
+    #Logger.info(INDENT_OUTPUT(4, "item_count=%d" % (item_count)))
+    items = {}
+    for item_index in range(item_count):
+      item_key = self._string_or_key()
+      item_type = self._readf(">4s")[0]
+      if not item_type in _desc_item_factory:
+        Logger.info(INDENT_OUTPUT(4, "unknown descriptor item '%s', skipping ahead." % item_type))
+        break
 
-        class_id_name = _unicode_string()
-        class_id = _string_or_key()
-        Logger.info(INDENT_OUTPUT(4, u"name='%s' clsid='%s'" % (class_id_name, class_id)))
-
-        item_count = self._readf(">L")[0]
-        #Logger.info(INDENT_OUTPUT(4, "item_count=%d" % (item_count)))
-        items = {}
-        for item_index in range(item_count):
-            item_key = _string_or_key()
-            item_type = self._readf(">4s")[0]
-            if not item_type in _desc_item_factory:
-                Logger.info(INDENT_OUTPUT(4, "unknown descriptor item '%s', skipping ahead." % item_type))
-                break
-
-            items[item_key] = _desc_item_factory[item_type]()
-            #Logger.info(INDENT_OUTPUT(4, "item['%s']='%r'" % (item_key,items[item_key])))
-            #print items[item_key]
-        return items
+      items[item_key] = _desc_item_factory[item_type]()
+      #Logger.info(INDENT_OUTPUT(4, "item['%s']='%r'" % (item_key,items[item_key])))
+      #print items[item_key]
+    return items
