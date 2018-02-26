1fd934da9932d7327eec328c54d9366479375db8
RichardTang <e5527c@gmail.com>
2018-02-26 23:04:59 +0800

    add -3 support

--- psd_channel_suffixes.py
+++ psd_channel_suffixes.py
@@ -5,8 +5,19 @@ sys.setdefaultencoding("utf-8")
 
 """
 Header mode field meanings
+
+number of channels
+
+Channel information. Six bytes per channel, consisting of:
+
+2 bytes for Channel ID: 0 = red, 1 = green, etc.;
+
+-1 = transparency mask; -2 = user supplied layer mask, -3 real user supplied layer mask (when both a user mask and a vector mask are present)
+
+
 """
 CHANNEL_SUFFIXES = {
+    -3: 'real layer mask',
     -2: 'layer mask',
     -1: 'A',
     0: 'R',
