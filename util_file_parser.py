b7c200245f772613ec9a7e14dcd02adaf4ac66fc
RichardTang <e5527c@gmail.com>
2018-02-27 11:34:50 +0800

    remove debug info

--- util_file_parser.py
+++ util_file_parser.py
@@ -14,17 +14,17 @@ from util_indent_output import INDENT_OUTPUT
 
 class FileParser(object):
 
-    def _readf(self, format):
-        """read a strct from file structure according to format"""
-        return unpack(format, self.fd.read(calcsize(format)))
+  def _readf(self, format):
+    """read a strct from file structure according to format"""
+    return unpack(format, self.fd.read(calcsize(format)))
 
-    def _skip_block(self, desc, indent=0, new_line=False):
-        (n,) = self._readf('>L') # (n,) is a 1-tuple.
-        if n:
-            self.fd.seek(n, 1) # 1: relative
+  def _skip_block(self, desc, indent=0, new_line=False):
+    (n,) = self._readf('>L') # (n,) is a 1-tuple.
+    if n:
+      self.fd.seek(n, 1) # 1: relative
 
-        if new_line:
-            Logger.info('')
-        Logger.info(INDENT_OUTPUT(indent, 'Skipped %s with %s bytes' % (desc, n)))
+    if new_line:
+      Logger.info('')
+    #Logger.info(INDENT_OUTPUT(indent, 'Skipped %s with %s bytes' % (desc, n)))
 
 
