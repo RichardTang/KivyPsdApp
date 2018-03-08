1257c6b28fc6d39c372c423a38e09b72150713c6
Richard Tang <tang_can@qq.com>
2018-03-08 17:29:10 +0800

    change to border image

--- my_image_button.py
+++ my_image_button.py
@@ -2,15 +2,21 @@ from kivy.app import App
 from kivy.uix.image import Image
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.behaviors import ToggleButtonBehavior
+from kivy.graphics import BorderImage
 
-class MyImageButton(ToggleButtonBehavior, Image):
+
+class MyImageButton(ToggleButtonBehavior, BorderImage):
   def __init__(self, **kwargs):
     super(MyImageButton, self).__init__(**kwargs)
-    self.source = 'atlas://data/images/defaulttheme/checkbox_off'
+    #self.source = 'atlas://data/images/defaulttheme/checkbox_off'
 
   def on_state(self, widget, value):
     if value == 'down':
-      self.source = 'atlas://data/images/defaulttheme/checkbox_on'
+      #self.source = 'atlas://data/images/defaulttheme/checkbox_on'
+      #border=(1,1,1,1)
+      pass
     else:
-      self.source = 'atlas://data/images/defaulttheme/checkbox_off'
+      #self.source = 'atlas://data/images/defaulttheme/checkbox_off'
+      #border=(0,0,0,0)
+      pass
 
